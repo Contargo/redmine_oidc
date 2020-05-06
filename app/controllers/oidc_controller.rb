dd5ba78fb92ddff0663354474c0c52f2141d1891
Florian Krupicka <krupicka@synyx.de>
2020-05-06 17:55:55 +0200

    Initial controller mockup for OIDC login

---
+++ app/controllers/oidc_controller.rb
@@ -0,0 +1,70 @@
+require 'openid_connect'
+
+class OidcController < ApplicationController
+
+  ISSUER_URL = 'https://example.com/auth/realms/example'
+
+  CLIENT_ID = 'client-id'
+  CLIENT_SECRET = 'client-secret'
+
+  def login
+    oidc_client.redirect_uri = oidc_callback_url
+
+    nonce = new_nonce
+
+    redirect_to oidc_client.authorization_uri(
+        nonce: nonce,
+        state: nonce,
+        scope: oidc_config.scopes_supported & [:openid, :email, :profile, :address].collect(&:to_s),
+    )
+  end
+
+  def callback
+    oidc_client.redirect_uri = oidc_callback_url
+    oidc_client.authorization_code = params[:code]
+    access_token = oidc_client.access_token!
+    _id_token_ = decode_id access_token.id_token
+    _id_token_.verify!(
+        issuer: ISSUER_URL,
+        client_id: CLIENT_ID,
+        nonce: stored_nonce
+    )
+    @id_token = _id_token_
+  end
+  private
+
+  def oidc_client
+    @oidc_client ||= OpenIDConnect::Client.new(
+        identifier: CLIENT_ID,
+        secret: CLIENT_SECRET,
+        authorization_endpoint: oidc_config.authorization_endpoint,
+        token_endpoint: oidc_config.token_endpoint,
+        userinfo_endpoint: oidc_config.userinfo_endpoint,
+        jwks_uri: oidc_config.jwks_uri,
+        scopes_supported: oidc_config.scopes_supported
+    )
+  end
+
+  def oidc_config
+    @oidc_config ||= OpenIDConnect::Discovery::Provider::Config.discover! ISSUER_URL
+
+  rescue OpenIDConnect::Discovery::DiscoveryFailed => e
+
+
+  end
+
+  def new_nonce
+    session[:nonce] = SecureRandom.hex(16)
+  end
+
+  def stored_nonce
+    n = session[:nonce]
+    session.delete(:nonce)
+    n
+  end
+
+  def decode_id(id_token)
+    OpenIDConnect::ResponseObject::IdToken.decode id_token, oidc_config.jwks
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+
+Rails.application.routes.draw do
+  get 'oidc/login', :to => 'oidc#login', :as => 'oidc_login'
+  get 'oidc/callback', :to => 'oidc#callback', :as => 'oidc_callback'
+end
