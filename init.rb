b1ab4858dc559229daf33b0f430a1a7166925912
Florian Krupicka <krupicka@synyx.de>
2020-05-06 15:20:58 +0200

    Initially generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_oidc do
+  name 'Redmine OpenId Connect plugin'
+  author 'Contargo GmbH & Co. KG'
+  description 'Add login with OpenId Connect as another login option'
+  version '0.0.1'
+  url 'https://github.com/Contargo/redmine_oidc'
+  author_url 'https://contargo.net'
+end
