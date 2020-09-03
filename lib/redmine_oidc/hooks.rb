cb7248facb177849bf457d5f38bd9b37d6161632
Joachim Mathes <jmathes@contargo.net>
2020-09-03 17:47:29 +0200

    Remove obsolete hook

--- lib/redmine_oidc/hooks.rb
+++
@@ -1,9 +0,0 @@
-# OpenID Connect Authentication for Redmine
-# Copyright (C) 2020 Contargo GmbH & Co. KG
-#
-module RedmineOidc
-  class Hooks < Redmine::Hook::ViewListener
-    render_on :view_account_login_bottom,
-              :partial => 'hooks/oidc_login'
-  end
-end
