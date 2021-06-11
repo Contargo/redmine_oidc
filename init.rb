240a4b592824acd92bb7ea2c6a2ae3b4ae267928
Joachim Mathes <jmathes@contargo.net>
2021-06-11 16:13:28 +0200

    Raise version to 1.0.0

--- init.rb
+++ init.rb
@@ -21,7 +21,7 @@ Redmine::Plugin.register :redmine_oidc do
   name 'Redmine OpenId Connect plugin'
   author 'Contargo GmbH & Co. KG'
   description 'Add login with OpenId Connect as another login option'
-  version '0.0.1'
+  version '1.0.0'
   url 'https://github.com/Contargo/redmine_oidc'
   author_url 'https://contargo.net'
   settings :default => {:enabled => false},
